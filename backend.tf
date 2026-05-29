ac03d9b114ff3d141d7d5a1e49740677f5a770e9
2kita9 <likhitha.sivapuram@gmail.com>
2026-05-29 14:22:36 +0530

    done

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.5.7"
 
   backend "s3" {
-    bucket = "cmtr-qxgoe9r5-backend-new-bucket-1780043416"
-    key    = "tf_code.tfstate"
+    bucket = var.bucket_name
+    key    = var.key_name
     region = "eu-west-1"
   }
 
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "bucket_name" {
+    description = "val of bucket"
+    type = string
+    default = "cmtr-qxgoe9r5-backend-bucket-1780043416"
+  
+}
+
+variable "key_name" {
+    description = "val of key"
+    type = string
+    default = "tf_code.tfstate"
+  
+}
