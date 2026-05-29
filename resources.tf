e4d9f99033e783129cf97bfc40c63d9895c911b7
2kita9 <likhitha.sivapuram@gmail.com>
2026-05-29 14:29:30 +0530

    fix backend migration

---
+++ resources.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_policy" "custom_policy" {
+  name = "custom_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:*"]
+        Resource = "*"
+      }
+    ]
+  })
+}
